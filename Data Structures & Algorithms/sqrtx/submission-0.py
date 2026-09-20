baa033623a1acd3015ca8f9af6051c45cea656a4
Varshith Reddy.M <163147457+varshithreddy39@users.noreply.github.com>
2026-09-20 20:58:32 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/sqrtx/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low=0
+        high=x//2
+
+        while low<=high:
+            mid=(low+high)//2
+            if mid*mid<=x:
+                low=mid+1
+            else:
+                high=mid-1
+        return high
+        
